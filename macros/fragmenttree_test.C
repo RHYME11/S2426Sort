1bdd04c84ae1192674b2bd04285654ee232f9192
RHYME11 <amaydaygo@gmail.com>
2026-08-20 13:24:27 -0700

    edit macros

--- macros/fragmenttree_test.C
+++ macros/fragmenttree_test.C
@@ -7,23 +7,17 @@
   long nentries = FragmentTree->GetEntries();                                                      
   long x = 0;
 
-  TH1D *hpileup = new TH1D("hpileup", "Core: # of pile up", 100,0,100);
-  TH2D *h[64];
+  TH1D *h[64];
   for(int i=0;i<64;i++){
-    h[i] = new TH2D(Form("h%i",i),Form("KValue vs Energy at Array%i",i),500,0,500,4e3,0,4e3);
+    h[i] = new TH1D(Form("h%i",i),Form("Core: dt = fabs(Time()-TimestampNs()) Array%i",i),4e6,0,4e6);
   } 
   int count0, count1, count2, count3; 
   for(x;x<nentries;x++){                                                                         
     FragmentTree->GetEntry(x);
-    long thisTime = ftg->TimestampNs();
     if(ftg->DetType()!=0) continue;
-    count0++;
     int arrynumber = ftg->Number()/15 + 16;
-    h[arrynumber]->Fill(ftg->KValue(), ftg->Energy());
-    if(ftg->KValue()==379) count1++;
-    if(ftg->Pileup() == 0) count2++; 
-    if(ftg->Pileup() == 1) count3++;
-    hpileup->Fill(ftg->Pileup()); 
+    double dt = fabs(ftg->Time()-ftg->TimestampNs());
+    h[arrynumber]->Fill(dt);
   }
 
   TCanvas *c = new TCanvas;
@@ -34,11 +28,5 @@
     h[i]->Draw();
     c->Update();
   }
-  new TCanvas; hpileup->Draw();
-
-  printf("C(core) = %i\n",count0);
-  printf("C(core: KValue=379) = %i\n",count1);
-  printf("C(core: Pileup=0)   = %i\n",count2);
-  printf("C(core: Pileup=1)   = %i\n",count3);
   
 }
