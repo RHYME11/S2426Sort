e7a3e2f0e9177b9a79c8549984bdc771d6dce392
RHYME11 <amaydaygo@gmail.com>
2026-09-10 11:52:44 -0700

    edit macros

--- macros/eventtree_test.C
+++ macros/eventtree_test.C
@@ -6,13 +6,15 @@
   long nentries = EventTree->GetEntries();
   long x = 0;
   int count0, count1, count2, count3,count4, count5,count6, count7,count8, count9,count10;
+  std::vector<int> vemt;
   for(x;x<nentries;x++){
     EventTree->GetEntry(x);
     bool emv = false;
     bool emt = false;
+    int count = 0;
     for(int m=0;m<eve->Fragments().size();m++){
       int dettype = eve->Fragments()[m].DetType();
-      if(dettype == 8) emt = true;
+      if(dettype == 8) {emt = true; count++;}
       if(dettype ==13 || dettype == 14) emv = true;
     }
     if(emv){
@@ -20,13 +22,13 @@
       if(emt) count1++;
       else count2++;
     }
-    if(emt && !emv) count3++;
+    if(emt && !emv) {count3++; vemt.push_back(count);printf("entry = %lu\n",x);}
   }
 
   printf("total events           = %lu\n",EventTree->GetEntries());
-  printf("events have emv        = %d\n", count0);
-  printf("events have emv && emt = %d\n", count1);
-  printf("events have emv no emt = %d\n", count2);
-  printf("events have emt no emv = %d\n", count3);
+  printf("events have vme        = %d\n", count0);
+  printf("events have vme && emt = %d\n", count1);
+  printf("events have vme no emt = %d\n", count2);
+  printf("events have emt no vme = %d\n", count3);
 
 }
--- macros/fragmenttree_test.C
+++ macros/fragmenttree_test.C
@@ -16,49 +16,26 @@
   }
  
   std::map<double, std::vector<double>> prompt;
+  for(double t:emt) prompt[t] = {};
   std::vector<std::vector<double>> delay; 
-  std::vector<double> tmp_delay;
   int j=0;
   for(int i=0;i<emt.size();i++){
-    double reft0 = emt[i];
-    prompt[reft0] = {};
-    if(!tmp_delay.empty()){
-      delay.push_back(tmp_delay);
+    double reft = emt[i];
+    std::vector<double> tmp_delay;
+    while(j<core.size() && core[j]-reft<-1500){
+      tmp_delay.push_back(core[j++]);
     }
-    tmp_delay.clear();
-    while(j<core.size()){
-      double coret = core[j];
-      double dt0   = coret - reft0 ;
-      if(dt0<-1500){
-        tmp_delay.push_back(coret);
-      }else if(fabs(dt0)<=1500){
-        prompt[reft0].push_back(coret);
-      }else{
-        break;
-      }
-      j++;
+    if(!tmp_delay.empty()) delay.push_back(tmp_delay);
+    while(j<core.size() && core[j]-reft<=1500){
+      prompt[reft].push_back(core[j++]);
     }
   }
-  if(!tmp_delay.empty()){
-    delay.push_back(tmp_delay);
-  }
-  tmp_delay.clear();
-  for(j;j<core.size();j++) tmp_delay.push_back(core[j]);
-  if(!tmp_delay.empty()){
-    delay.push_back(tmp_delay);
-  }
-   
-  //std::sort(vvme.begin(),vvme.end());
-  //vvme.erase(std::unique(vvme.begin(),vvme.end()),vvme.end());
-  //std::sort(vemt.begin(),vemt.end());
-  //vemt.erase(std::unique(vemt.begin(),vemt.end()),vemt.end());
+  if(j<core.size()) delay.emplace_back(core.begin()+j,core.end());
+  
   printf("count of emt  = %i\n",emt.size());
   printf("count of core = %i\n",core.size());
   printf("count of delay events = %i\n",delay.size());
-  TH1D *hs = new TH1D("hs", "core size correlated to emt", 100,0,100);
-  for(auto it=prompt.begin();it!=prompt.end();it++){
-    hs->Fill(it->second.size());
-  } 
-  new TCanvas;
-  hs->Draw();
+    
+
 }
+  
